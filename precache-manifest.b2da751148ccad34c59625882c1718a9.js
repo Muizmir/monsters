27ce85cb1f5b3dc24e5bd6a7dbde053c7be65d2c
Muiz <m.muiz.q@gmail.com>
2019-10-14 18:16:16 +1100

    Updates

---
+++ precache-manifest.b2da751148ccad34c59625882c1718a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad4fa6907451d33b056d9c1edf90eaa",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a5ee8f3197c6ff0abf48",
+    "url": "/monsters/static/css/main.7bf03350.chunk.css"
+  },
+  {
+    "revision": "a7ffd671cb21d8ff7fb4",
+    "url": "/monsters/static/js/2.fe5198a4.chunk.js"
+  },
+  {
+    "revision": "a5ee8f3197c6ff0abf48",
+    "url": "/monsters/static/js/main.0aa9e27d.chunk.js"
+  },
+  {
+    "revision": "b3b86a0e1a4861e794bd",
+    "url": "/monsters/static/js/runtime-main.e6f231b0.js"
+  }
+]);
