5bb6f3411c915215412c59421581f2fb62b904da
Muiz <m.muiz.q@gmail.com>
2019-10-15 10:45:41 +1100

    changed css grid layout

--- src/components/cards/card.component.js
+++ src/components/cards/card.component.js
@@ -5,9 +5,9 @@ export const Card = (props) => (
     <div className = 'card-container'>
         <img
              alt = "monster" 
-             src = {`https://robohash.org/${props.monster.id}?set=set2&size=150x180`}
+             src = {`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
              />
         <h3> {props.monster.name} </h3>
-        <h6> {props.monster.email} </h6>
+        <h5> {props.monster.email} </h5>
     </div>
 )
